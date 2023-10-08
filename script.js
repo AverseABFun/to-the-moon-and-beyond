74984352d06f813bc20c5887265791b26adb7303
AverseABFun <averse.abfun@gmail.com>
2023-10-08 08:32:00 -0500

    misc stuff

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ console.log(
   'I said nothing to see here!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nWell hello there...\nNothing to see here...'
 );
 
-window.gameVars = {};
+window.gameVars = JSON.parse(window.localStorage.getItem('gameVars') || '{}');
 
 if (!document.location.hash) {
   document.location.hash = document.querySelector('.show-start').id;
   document.location.reload();
 } else {
+  check_var();
   document.querySelector(document.location.hash).style.display = 'block';
   var func = function () {
     document.querySelector(document.location.hash).style.opacity =
@@ -27,6 +28,17 @@ if (!document.location.hash) {
     document.location.hash
   ) {
     document.querySelector('.title').style.display = 'none';
+  }
+}
+
+function check_var() {
+  var check_vars = Array.from(document.querySelectorAll('.check-var'));
+  for (var i = 0; i < check_vars.length; i++) {
+    console.log(i);
+    var item = check_vars[i];
+    if (gameVars[item.dataset.var] != item.dataset.value) {
+      item.style.display = 'none';
+    }
   }
 }
 
@@ -60,17 +72,18 @@ for (var i in includes) {
 var buttons_change = Array.from(document.querySelectorAll('.change-page'));
 for (var i in buttons_change) {
   var item = buttons_change[i];
-  item.addEventListener('click', change_page_wrapper(item.dataset.toPage));
   if (item.classList.contains('set-var')) {
     (function (item) {
       item.addEventListener('click', function () {
         gameVars[item.dataset.var] = item.dataset.value;
+        window.localStorage.setItem('gameVars', JSON.stringify(gameVars));
         console.log(
           `setted gameVars[${item.dataset.var}] = ${item.dataset.value}`
         );
       });
     })(item);
   }
+  item.addEventListener('click', change_page_wrapper(item.dataset.toPage));
 }
 
 var ending_divs = Array.from(document.querySelectorAll('.ending'));
